73b20e457fb3e618c2a430309ba548ff60c13c4a
Lester Lee <ll6@williams.edu>
2018-10-10 23:17:11 -0400

    Add repeating code

--- repetition.js
+++ repetition.js
@@ -1,8 +1,14 @@
 var $buttons = $('.Button');
+var numClicks = 0;
 
 function replaceColors() {
+  //clear text
+  $buttons.text('');
+
+  //choose random button
   let $randbtn = $($buttons[Math.floor(Math.random() * 8)]);
 
+  //choose random colors
   let color1 = chroma.random();
   let color2 = chroma.random();
   while (chroma.contrast(color1, color2) < 4.5) {
@@ -10,19 +16,39 @@ function replaceColors() {
   }
   let color3 = chroma.mix(color1, color2);
 
+  //apply corresponding css
   $buttons.css({
     'box-shadow': '0 0 1.5em 4px ' + color1,
     'background-color': color1
   });
+
   $randbtn.css({
     'box-shadow': '0 0 1.5em 4px ' + color2,
-    'background-color': color2
+    'background-color': color2,
+    'color': color1
   });
-  $('body').css('background-color', color3);
+  $randbtn.addClass('--alt');
+  $randbtn.text(numClicks);
 
+  $('body').css('background-color', color3);
 }
 
 $(function () {
+  let repeated = false;
   replaceColors();
-  $buttons.on('click', replaceColors);
+  $buttons.on('click', function(){
+    numClicks++;
+    if (numClicks === 11){
+      alert('Hello.');
+    }
+    if (numClicks === 44 && !repeated){
+      numClicks = 40;
+      repeated = true;
+    }
+    if (numClicks === 88){
+      alert();
+      numClicks = 0;
+    }
+    replaceColors();
+  });
 });
